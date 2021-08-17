1753ebad19cd3f87d4cfad882c5f14e1975fb883
Joel Meyerson <jrmeyerson@gmail.com>
2021-08-17 13:23:05 -0400

    added star file metadata loading

--- README.md
+++ README.md
@@ -2,15 +2,13 @@
 The purpose of the script is to visualize statistics on the conformational composition of protein homo-oligomers in cryo-EM data. Particles must be processed by symmetry expansion to generate masked images of each protein subunit from every particle in the dataset. The subunits must then be subjected to 3D conformational classification to identify the conformation of each subunit image. The script uses the resulting conformational assignments for the protein subunits to determine the conformational composition of each protein homo-oligomer.
 
 ### File inputs
-The script assumes there are two interpretable subunit conformations (C1 and C2) and a third subunit conformation that is not interpretable but must be accounted for in the analysis (C3).
+The script assumes there are two interpretable subunit conformations (C1 and C2). These two subsets need not sum to the total number of subunits in the dataset. This allows for a third subset of subunits which may represent an uninterpretable conformation or conformations. The script will automatically handle any subunits which are not accounted for in C1 and C2.
 
 `sym.star` STAR file with symmetry expanded particles.
 
 `c1.star` STAR file with particles for conformation 1.
 
 `c2.star` STAR file with particles for conformation 2.
-
-`c3.star` STAR file with other particles that are not assigned to conformation 1 or 2.
 
 ### Conda environment
 Quick setup for a conda environment to run the script.
@@ -22,9 +20,9 @@ Quick setup for a conda environment to run the script.
 `conda activate ci` # activate environment
 
 ### Running the script
-Command to run the script with file inputs and the number of subunits specified. In this example the number of subunits is four. The output is `out.png` and is written to the user working directory.
+Command to run the script with file inputs and the number of subunits per protein specified. In this example the number of subunits is four. The output is `out.png` and is written to the user working directory.
 
-`python ci.py -n 4 -i sym.star -c1 c1.star -c2 c2.star -c3 c3.star`
+`python ci.py -n 4 -i sym.star -c1 c1.star -c2 c2.star`
 
 
 
--- ci.py
+++ ci.py
@@ -1,35 +1,109 @@
-import os
-import shutil
 import argparse
-import sys
 import re
 import matplotlib.pyplot as plt
-from progress.bar import Bar
+from progress.spinner import Spinner
 
 def main():
     
     # parse arguments
     parser = argparse.ArgumentParser(description='Analyze conformational compositions of protein particles in cryo-EM data.')
-    parser.add_argument('-i', '--input', type=str, help='star file with symmetry expanded particles', required=True)
-    #parser.add_argument('-c1', '--conformation1', type=str, help='star file with particles in subunit conformation 1', required=True)
-    #parser.add_argument('-c2', '--conformation2', type=str, help='star file with particles in subunit conformation 2', required=True)
-    #parser.add_argument('-c3', '--conformation3', type=str, help='star file with particles in undefined conformation(s)', required=True)
-    #parser.add_argument('-s', '--symmetry', type=str, help='number of subunits', required=True)
+    parser.add_argument('-s', '--symexpand', type=str, help='star file with symmetry expanded particles', required=True)
+    parser.add_argument('-c1', '--conformation1', type=str, help='star file with particles in subunit conformation 1', required=True)
+    parser.add_argument('-c2', '--conformation2', type=str, help='star file with particles in subunit conformation 2', required=True)
+    parser.add_argument('-n', '--numsubunits', type=int, help='number of subunits', required=True)
     args = parser.parse_args()
     
-    cnt = countpar(args.input)
-    print(cnt)
+    # make list with particle oligomer name, x coordinate, y coordinate, and make list entries to store subunit counts
+    num_sub = args.numsubunits
+    [num_par_nx, num_par, meta_par] = get_par_meta(args.symexpand, num_sub)
     
-# count number of particles in a star file
-def countpar(starfile):
+    # make lists for subunits in c1 and c2
+    [num_par_c1, meta_c1] = get_sub_meta(args.conformation1, num_sub)
+    [num_par_c2, meta_c2] = get_sub_meta(args.conformation2, num_sub)
     
-    count = 0
+    # confirm that particle counts make sense
+    # if num_par % num_sub != 0:
+#         print("The number of particles in " + args.symexpand + " is not a multiple of " + str(num_sub) +". Exiting.")
+#         exit()
+#
+#     if (num_c1 > num_par or num_c2 > num_par):
+#         print("The number of particles in each of the STAR files for subunit conformations should be fewer than in the symmetry expanded STAR file. Exiting.")
+#         exit()
+#
+#     if (num_c1 + num_c2) > num_par:
+#         print("The number of total subunit conformation particles exceeds the symmetry expanded particles. Exiting.")
+#         exit()
+    
+# get relevant metadata from sym.star
+def get_par_meta(starfile, nsub):
+    
+    num_par_nx = 0 # total particle entries in sym.star
+    num_par = 0 # total non-redundant particle entries in sym.star
+    meta = []
     with open(starfile, "r") as file:
-        for line in file:
-            if re.search(r'@', line): # this is a data entry which needs to be converted from array to tab-delimited string
-                count += 1
-
-    return count
+        
+        # create progress spinner
+        with Spinner('Reading metadata from ' + starfile + ' ') as spinner:
+        
+            for line in file:
+                # check if line has particle and that column indexes have been found
+                if (re.search(r'@', line) and img_col and x_col and y_col):            
+                    if ((num_par_nx + nsub) % nsub == 0): # skip redundant lines in sym.star
+                        parname = line.split()[img_col-1]
+                        x = line.split()[x_col-1]
+                        y = line.split()[y_col-1]
+                        meta.append([parname, x, y, 0, 0, 0]) # final three list entries will be used to store subunit counts
+                        if (num_par % 10000 == 0): # slow spinner
+                            spinner.next()
+                        num_par += 1
+                    num_par_nx += 1
+    
+                # find header entry with column number for particle name
+                elif re.search(r'_rlnImageName', line):
+                    img_col = int(str(line.split()[1]).strip("#"))
+        
+                # find header entry with column number for particle name
+                elif re.search(r'_rlnCoordinateX', line):
+                    x_col = int(str(line.split()[1]).strip("#"))
+        
+                # find header entry with column number for particle name
+                elif re.search(r'_rlnCoordinateY', line):
+                    y_col = int(str(line.split()[1]).strip("#"))
+        
+    if len(meta) == 0:
+        print("STAR file " + starfile + " is missing essential metadata or has no particles. Exiting.")
+        exit()
+    else:
+        return [num_par_nx, num_par, meta]
+            
+# get relevant metadata from sym.star
+def get_sub_meta(starfile, nsub):
+    
+    num_par = 0 # total particle entries in star
+    meta = []
+    with open(starfile, "r") as file:
+        
+        # create progress spinner
+        with Spinner('Readingmeta data from ' + starfile + ' ') as spinner:
+            
+            for line in file:  
+                # check if line has particle and that img column index has been found
+                if (re.search(r'@', line) and img_col):
+                    parname = line.split()[img_col-1]
+                    meta.append(parname)
+                    if (num_par % (nsub*10000) == 0): # slow spinner
+                        spinner.next()
+                    num_par += 1
+                        
+                # find header entry with column number for particle name
+                elif re.search(r'_rlnImageName', line):
+                    img_col = int(str(line.split()[1]).strip("#"))
+        
+    if len(meta) == 0:
+        print("STAR file " + starfile + " is missing essential metadata or has no particles. Exiting.")
+        exit()
+    else:
+        return [num_par, meta]
 
 if __name__ == "__main__":
     main()
